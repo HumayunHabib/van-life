5b81f2489977a2008daf9abb52b1bb312f5b633c
HumayunHabib <humayunhabib786@gmail.com>
2024-08-25 18:46:54 +0500

    single route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Vans from "./pages/vans";
+import VanDetail from "./pages/VanDetail";
 
 function App() {
   return (
@@ -10,11 +12,14 @@ function App() {
         <Link to="/">#VANLIFE</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
